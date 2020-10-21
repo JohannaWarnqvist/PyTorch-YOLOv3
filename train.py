eb463a5626922c25c242c47fe763590f97c6ae44
JohannaWarnqvist <johwar@student.chalmers.se>
2020-10-21 14:15:58 +0200

    Update train.py to be for wildlife

--- train.py
+++ train.py
@@ -27,9 +27,9 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
     parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
-    parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
-    parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
-    parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
+    parser.add_argument("--model_def", type=str, default="config/yolov3_wildlife.cfg", help="path to model definition file")
+    parser.add_argument("--data_config", type=str, default="dataset/wildlife.data", help="path to data config file")
+    parser.add_argument("--pretrained_weights", type=str, default="weights/yolov3.weights", help="if specified starts from checkpoint model")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
     parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model weights")
@@ -109,11 +109,10 @@ if __name__ == "__main__":
                 # Accumulates gradient before each step
                 optimizer.step()
                 optimizer.zero_grad()
-            
-            
-        # ----------------
-        #   Log progress
-        # ----------------
+
+            # ----------------
+            #   Log progress
+            # ----------------
 
             log_str = "\n---- [Epoch %d/%d, Batch %d/%d] ----\n" % (epoch, opt.epochs, batch_i, len(dataloader))
 
@@ -147,7 +146,6 @@ if __name__ == "__main__":
             print(log_str)
 
             model.seen += imgs.size(0)
-            
 
         if epoch % opt.evaluation_interval == 0:
             print("\n---- Evaluating Model ----")
@@ -167,8 +165,6 @@ if __name__ == "__main__":
                 ("val_mAP", AP.mean()),
                 ("val_f1", f1.mean()),
             ]
-            
-            
             logger.list_of_scalars_summary(evaluation_metrics, epoch)
 
             # Print class APs and mAP
