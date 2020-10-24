a83ffa0e6bf9392a52b4401ca861b237b422c6bd
Ubuntu <johwarazure@dml-instance.vytuwta3tjgurdqf1e2xtq0k3a.ax.internal.cloudapp.net>
2020-10-24 20:47:45 +0000

    Data from run without augmentation

---
+++ divide_file.py
@@ -0,0 +1,13 @@
+lines_per_file = 100000
+smallfile = None
+with open('log_files/loss_without.txt') as bigfile:
+    for lineno, line in enumerate(bigfile):
+        if lineno % lines_per_file == 0:
+            if smallfile:
+                smallfile.close()
+            small_filename = 'loss_small_without_{}.txt'.format(lineno + lines_per_file)
+            smallfile = open(small_filename, "w")
+        smallfile.write(line)
+    if smallfile:
+        smallfile.close()
+
