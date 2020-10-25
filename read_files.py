ea0a8fcc39e1bc6fd0a418650d94222f472cb118
JohannaWarnqvist <johwar@student.chalmers.se>
2020-10-25 12:17:07 +0100

    Nicer looking plots
    Fixing the training plots to have labels and title. Also describe the functions better and give them clearer names.

--- read_files.py
+++ read_files.py
@@ -73,26 +73,49 @@ def read_loss(filenames, doublet_epoch=None, doublet_batch=0):
 	batches = batches - doublet_batch
 	return dict_batches, batches
 
-def plot_things(dict_batches, name, nr_batches):
+def plot_training_metric(dict_batches, metric, nr_batches, augmentation):
+	"""Plot a metric for each batch in each epoch.
+	   dict_batches is the dict with all data.
+	   metric is the name of the metric.
+	   nr_batches is the total number of batches over time.
+	   augmentation indicates if augmentation was used during training.
+	"""
 
-	plt.plot(range(nr_batches), [dict_batches[i][name] for i in range(nr_batches)])
-	plt.show()
+	plt.plot(range(nr_batches), [dict_batches[i][metric] for i in range(nr_batches)])
+	plt.xlabel("batches")
+	plt.ylabel(metric)
+	if augmentation:
+		plt.title(f"{metric} when training with augmented data")
+	else:
+		plt.title(f"{metric} when training without augmented data")
 
-def plot_average(dict_batches, name, nr_batches):
+def plot_average(dict_batches, metric, nr_batches, augmentation):
+	"""Plot average of a metric over different YOLO-layers for each batch in each epoch.
+	   dict_batches is the dict with all data.
+	   metric is the name of the metric.
+	   nr_batches is the total number of batches over time.
+	   augmentation indicates if augmentation was used during training.
+	"""
 
-	plt.plot(range(nr_batches), [sum([dict_batches[i]["" + name + "_" + str(j)] for j in range(1,4)])/3 for i in range(nr_batches)])
+	plt.plot(range(nr_batches), [sum([dict_batches[i]["" + metric + "_" + str(j)] for j in range(1,4)])/3 for i in range(nr_batches)])
+	plt.xlabel("batches")
+	plt.ylabel(metric)
+	if augmentation:
+		plt.title(f"Average {metric} when training with augmented data")
+	else:
+		plt.title(f"Average {metric} when training without augmented data")
+
 
 dict_batches, nr_batches = read_loss(["log_files/loss_small_without_1.txt", 
 									  "log_files/loss_small_without_2.txt",
 									  "log_files/loss_small_without_3.txt"
 									 ], 34, 115)
 
-
 plt.figure(0)
-plot_things(dict_batches, 'loss', nr_batches)
+plot_training_metric(dict_batches, 'loss', nr_batches, False)
 
 plt.figure(1)
-plot_average(dict_batches, 'precision', nr_batches)
+plot_average(dict_batches, 'precision', nr_batches, False)
 
 plt.show()
 
