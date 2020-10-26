0d4877fad3a5b618fe41226687f37817ded70295
JohannaWarnqvist <johwar@student.chalmers.se>
2020-10-26 14:15:15 +0100

    \\ -> /

--- change_files.py
+++ change_files.py
@@ -6,14 +6,14 @@ def change_file(filepath, new_file, new_string):
 	with open(new_file, 'w') as f: 
 	    f.write('\n'.join([new_string + line for line in lines]))  # write lines with '#' appended
 
-filepath = "C:\\Users\\Johanna\\Documents\\GitHub\\PyTorch-YOLOv3\\dataset\\images\\train.txt"
+filepath = "dataset/images/train.txt"
 new_file = "train.txt"
-new_string = "dataset\\images\\train\\"
+new_string = "dataset/images/train/"
 
 change_file(filepath, new_file, new_string)
 
-filepath = "C:\\Users\\Johanna\\Documents\\GitHub\\PyTorch-YOLOv3\\dataset\\images\\valid.txt"
+filepath = "dataset/images/valid.txt"
 new_file = "valid.txt"
-new_string = "dataset\\images\\valid\\"
+new_string = "dataset/images/valid/"
 
 change_file(filepath, new_file, new_string)
